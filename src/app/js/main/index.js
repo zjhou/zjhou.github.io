10df997ee7c879d970419f752e44cd0a81d591e9
zjhou <z@zjh.im>
2018-11-11 11:08:50 +0800

    fix: add 'install' command to install extra commands

--- src/app/js/main/index.js
+++ src/app/js/main/index.js
@@ -1,6 +1,13 @@
 import PseudoTerminal from 'pseudoterminal';
-import commands from '../commands';
+// import commands from '../commands';
 import {$} from '../utils';
 const Terminal = PseudoTerminal($('#app'));
-Terminal.commands  = commands;
+// Terminal.commands  = commands;
+Terminal.commands = {
+    install: async () => {
+        const {default: commands} = await import('../commands');
+        Terminal.addCommands(commands);
+        return Promise.resolve('成功安装额外命令，可输入 help 查看全部可用命令');
+    }
+};
 export {Terminal};
