9f90675283d028663ac77e3a8fe3de53dc14a752
zjhou <z@zjh.im>
2018-11-14 22:55:13 +0800

    feat: auto exec install command

--- src/app/js/main/index.js
+++ src/app/js/main/index.js
@@ -5,7 +5,9 @@ Terminal.commands = {
     install: async () => {
         const {default: commands} = await import('../commands');
         Terminal.addCommands(commands);
-        return Promise.resolve('成功安装额外命令，可输入 help 查看全部可用命令');
+        return Promise.resolve('成功安装命令，可输入 help 查看');
     }
 };
+
+Terminal.humanizerExec('install');
 export {Terminal};
