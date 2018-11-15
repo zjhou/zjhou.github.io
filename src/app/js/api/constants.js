ef5598bf479e7ef328dfdffd43efc41d8c790342
zjhou <z@zjh.im>
2018-11-15 13:57:45 +0800

    fix: add posts api uri

--- src/app/js/api/constants.js
+++ src/app/js/api/constants.js
@@ -1,4 +1,13 @@
 const apiEndPoint = 'https://api.fufu.im/graphql';
+const myEndPoint = 'https://api.zjh.im';
+const api = {
+    posts: `${myEndPoint}/posts`,
+    post: `${myEndPoint}/posts/{0}`
+};
+
 export {
+    api,
     apiEndPoint
 };
+
+
