0dbe2a2079c69e107eabc619697937f8ffeed7cd
zjhou <z@zjh.im>
2018-11-15 14:00:03 +0800

    feat: add tpl func

---
+++ src/app/tpl/post.js
@@ -0,0 +1,5 @@
+const title = ({title, _id}) =>
+    `<span class="command" data-cmd="cat ${title}" data-id="${_id}">${title}</span>`;
+`<span class="command" data-cmd="cat ${title}" data-id="${_id}">${title}</span>`;
+
+export {title};
