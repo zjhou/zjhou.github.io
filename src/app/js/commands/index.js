b94b03938adf88460bbce4ffbf6b96d737ff2b2e
zjhou <z@zjh.im>
2018-11-22 19:31:28 +0800

    chore: rename rexec to rexe

--- src/app/js/commands/index.js
+++ src/app/js/commands/index.js
@@ -1,18 +1,18 @@
 import about from './about';
 import ls from './ls';
-import rexec from './rexec';
+import rexe from './rexe';
 import tohtml from './tohtml';
 import cat from './cat';
 import {MOON_URL} from '../constants/strVar';
 import {goto} from '../utils';
 
 const commands = {
-    about,
     moon: () => {goto(MOON_URL);},
+    about,
     ls,
     cat,
     tohtml,
-    rexec
+    rexe
 };
 
 export default commands;
---
+++ src/app/js/commands/rexe.js
@@ -0,0 +1,16 @@
+import {execCmdRemote} from '../api';
+import {docTpl} from '../utils';
+
+const docDesc =
+    `rexe 能远程执行命令，目前只只支持 ls, cat；如：
+        rexe ls
+        rexe cat <file name>`
+
+const doc = docTpl('rexe - Run command at remote machine', 'rexe [command]', docDesc);
+export default {
+    handler: async (a, b, c, {restParams}) =>{
+        const {result, error} = await execCmdRemote(restParams);
+        return error || result;
+    },
+    doc
+}
